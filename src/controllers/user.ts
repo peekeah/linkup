3569c6a25289dc26caf7487ee1499729ee69a30b
Pranay Kothari <pranay.kothari109@gmail.com>
2024-11-09 14:34:32 +0530

    Add user methods

---
+++ src/controllers/user.ts
@@ -0,0 +1,62 @@
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  mobile: string;
+  password: string;
+  address: IAddress
+}
+
+interface IAddress {
+  street: string;
+  area: string;
+  city: string;
+  state: string;
+  country: string;
+}
+
+let globalUserId = 1;
+
+class User {
+  private users: IUser[];
+
+  constructor() {
+    this.users = []
+  }
+
+  create(user: IUser) {
+    this.users.push({
+      ...user,
+      id: globalUserId++,
+    })
+  }
+
+  update(user: IUser) {
+    const id = this.users.findIndex(({ id }) => id === user.id)
+
+    if (!id) {
+      throw new Error("user not found")
+    }
+
+    this.users[id] = user
+  }
+
+  delete(id: number) {
+    this.users = this.users.filter(user => user.id !== id)
+  }
+
+  getUsers() {
+    return this.users;
+  }
+
+  getUser(id: number) {
+    const user = this.users.find(el => el.id === id)
+
+    if (!user) {
+      throw new Error("user not found")
+    }
+
+    return user;
+
+  }
+}
