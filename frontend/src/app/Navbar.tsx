49a66e7c5bf62ff40d96c1e4d0d72f64bec495bd
Pranay Kothari <pranay.kothari109@gmail.com>
2024-11-20 16:14:09 +0530

    Initial frontend setup

---
+++ frontend/src/app/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-5">
+      <div>Logo</div>
+      <div className="flex justify-between gap-3">
+        <div>Home</div>
+        <div>About</div>
+        <div>Login</div>
+        <div>Sign up</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar;
